b05cd2dc0d4a1c414db56b40caef10c7d6cad06d
eliogovea <eliogovea1993@gmail.com>
2024-03-09 10:49:41 +0000

    Deploying to gh-pages from @ eliogovea/dev-env-cpp@ae9690a9a79ff196831b7b8bfd8f85e5f6ffe9aa 🚀

---
+++ master/documentation/search/files_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['power_2ehpp_9',['power.hpp',['../power_8hpp.html',1,'']]],
+  ['power_5fapp_2ecpp_10',['power_app.cpp',['../power__app_8cpp.html',1,'']]],
+  ['power_5ftest_2ecpp_11',['power_test.cpp',['../power__test_8cpp.html',1,'']]]
+];
---
+++ master/documentation/search/namespaces_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['math_8',['Math',['../namespaceMath.html',1,'']]]
+];
